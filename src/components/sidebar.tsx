5debb364b6a58495754f04a703cd007a846264e9
mhandlon <michael.handlon@gmail.com>
2025-11-16 17:19:17 -0500

    add sidebar component: integrate react-burger-menu and styling

---
+++ src/components/sidebar.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+import { slide as Menu } from "react-burger-menu"
+import { Link } from 'react-router-dom'
+
+const SideBar: React.FC = () => {
+    return (
+        <Menu>
+            <Link to='/'><span className="menu-text">Home</span></Link>
+            <Link to='/about'><span className="menu-text">About</span></Link>
+            <Link to="/open-source"><span className="menu-text">Open Source</span></Link>
+            <Link to="/volunteer"><span className="menu-text">Volunteer Work</span></Link>
+            <a className="menu-item" href="#contact"><span className="menu-text">Contact</span></a>
+        </Menu>
+    );
+};
+
+export default SideBar;
