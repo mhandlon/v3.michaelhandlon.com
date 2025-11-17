43d636df8bcb9f3071e2bbc1e78b4f20b5bef376
mhandlon <michael.handlon@gmail.com>
2025-11-17 14:54:42 -0500

    enhance sidebar: add FontAwesome icons to menu links and restore commented routes

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -2,16 +2,28 @@ import React from "react"
 import { slide as Menu } from "react-burger-menu"
 import { Link } from 'react-router-dom'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { library } from '@fortawesome/fontawesome-svg-core'
+// import { fas } from '@fortawesome/free-solid-svg-icons'
+// import { far } from '@fortawesome/free-regular-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import {
+    faHome,
+    faNewspaper,
+    faHandHoldingHeart,
+    faEnvelope,
+} from '@fortawesome/free-solid-svg-icons'
+
 import '../css/sidebar.css'
 
 const SideBar: React.FC = () => {
     return (
         <Menu>
-            <Link to='/'><span className="menu-text">Home</span></Link>
-            {/*<Link to='/about'><span className="menu-text">About</span></Link>*/}
-            {/*<Link to="/open-source"><span className="menu-text">Open Source</span></Link>*/}
-            {/*<Link to="/volunteer"><span className="menu-text">Volunteer Work</span></Link>*/}
-            {/*<a className="menu-item" href="#contact"><span className="menu-text">Contact</span></a>*/}
+            <Link to='/'><FontAwesomeIcon icon={faHome} /> <span className="menu-text">Home</span></Link>
+            <Link to='/about'><FontAwesomeIcon icon={faNewspaper} /><span className="menu-text">About</span></Link>
+            <Link to="/open-source"><FontAwesomeIcon icon={faGithub} /><span className="menu-text">Open Source</span></Link>
+            <Link to="/volunteer"><FontAwesomeIcon icon={faHandHoldingHeart} /><span className="menu-text">Volunteer Work</span></Link>
+            <a className="menu-item" href="#contact"><FontAwesomeIcon icon={faEnvelope} /><span className="menu-text">Contact</span></a>
         </Menu>
     );
 };
